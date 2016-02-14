4b6b1b438d60b01fc44596b1e4be13f625b131d2
dredivaris <dredivaris@gmail.com>
2016-02-14 16:53:37 -0500

    add placeholder user and sample template

--- app/views.py
+++ app/views.py
@@ -3,17 +3,24 @@ from flask import render_template
 from app import app
 
 
-# @app.route('/')
+# route to basic hello world
 @app.route('/hello')
 def hello_world_text():
     return "Hello, World!"
 
 
+# route to flask tutorial page
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    user = {'default_user': 'John Doe'}  # placeholder user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+    # return render_template('index.html')
 
 
+# route to react tutorial page
 @app.route('/hello_template')
 def hello():
     return render_template('hello.html')
