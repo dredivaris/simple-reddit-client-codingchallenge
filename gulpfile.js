e267cd05684f0e6832e00298bf600a354a1ab1e8
dredivaris <dredivaris@gmail.com>
2016-02-14 16:17:24 -0500

    add gulp transform and gulp clean task implementations, add main.js generated by gulp (from jsx) to hello.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 'use strict';
 
 // requirements
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    size = require('gulp-size'),
+    clean = require('gulp-clean');
+
+gulp.task('transform', function () {
+  return gulp.src('./app/static/scripts/jsx/main.js')
+    .pipe(browserify({transform: ['reactify']}))
+    .pipe(gulp.dest('./app/static/scripts/js'))
+    .pipe(size());
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['./app/static/scripts/js'], {read: false})
+    .pipe(clean());
+});
 
 gulp.task('default', function() {
-  console.log('hello!');
-
+  gulp.start('transform');
 });
