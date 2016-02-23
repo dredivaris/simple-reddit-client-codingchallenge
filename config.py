055261188f091a83646c29ea3ad45a936601e564
dredivaris <dredivaris@gmail.com>
2016-02-23 13:58:49 -0500

    add heroku db check & set

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "postgresql://redditdbuser:redajisdg@localhost/redditclient"
+if os.environ.get('HEROKU') is None:
+    SQLALCHEMY_DATABASE_URI = "postgresql://redditdbuser:redajisdg@localhost/redditclient"
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
